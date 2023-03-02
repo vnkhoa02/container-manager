795acc4d26d5901f357acee64efb1aeba3ef0088
khoavn <khoacaro96@gmail.com>
2023-03-03 01:43:23 +0700

    update code

---
+++ src/components/Header/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-header id="header">
+    <el-row align="middle">
+      <el-col :span="8">
+        <el-button :icon="Fold"
+                   @click="onOpenMenu"
+                   size="large"
+        />
+      </el-col>
+      <el-col :span="8">
+        <el-row justify="center">
+          <el-image style="width: 150px; height: 60px; background: transparent"
+                    src="https://dynamic.brandcrowd.com/asset/logo/3cfd6b07-267e-456b-9bb7-d5029cc6bb52/logo-search-grid-1x?logoTemplateVersion=1&v=637654796499570000"
+                    fit="fill"/>
+        </el-row>
+      </el-col>
+      <el-col :span="8">
+        <el-row justify="end">
+          <el-dropdown trigger="click">
+            <el-button
+                :icon="ArrowDown"
+                size="large"
+            >
+              Account
+            </el-button>
+            <template #dropdown>
+              <el-dropdown-menu>
+                <el-dropdown-item>Logout</el-dropdown-item>
+              </el-dropdown-menu>
+            </template>
+          </el-dropdown>
+        </el-row>
+      </el-col>
+    </el-row>
+  </el-header>
+</template>
+
+<script>
+import {ArrowDown, Expand, Fold} from '@element-plus/icons-vue'
+
+export default {
+  name: "Header",
+  data() {
+    return {
+      Fold,
+      Expand,
+      ArrowDown
+    }
+  },
+  methods: {
+    onOpenMenu() {
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+#header {
+  margin: 0;
+  padding: 0;
+  background-color: #089d9d;
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,18 +1,23 @@
 <template>
-  <el-container :direction="screenWidth <= 820 ? 'vertical' : 'horizontal'">
-    <Sidebar :screen-width="screenWidth" :screen-height="screenHeight"/>
-    <el-main>
-      <router-view/>
-    </el-main>
+  <el-container direction="vertical">
+    <Header ref="header" v-if="screenWidth > 820"/>
+    <el-container :direction="screenWidth <= 820 ? 'vertical' : 'horizontal'">
+      <Sidebar :screen-width="screenWidth" :screen-height="screenHeight"/>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
   </el-container>
 </template>
 
 <script>
 import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
 
 export default {
   name: "Index",
   components: {
+    Header,
     Sidebar
   },
   data() {
@@ -32,7 +37,7 @@ export default {
   methods: {
     updateScreenWidthHeight() {
       this.screenWidth = window.innerWidth
-      this.screenHeight = window.innerHeight
+      this.screenHeight = window.innerHeight - 60 // by default header has height = 60px
     }
   }
 }
